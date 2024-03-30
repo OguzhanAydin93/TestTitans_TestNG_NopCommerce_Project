7c405da14e33e834fffd362449981004551c992d
Degisiklikler1 <megaozi9517536@gmail.com>
2024-03-30 14:37:26 +0300

    hatalar giderildi ve US_504_Oguzhan test metodu açıldı

--- src/NopCommerce_Project/NopCommerce.java
+++ src/NopCommerce_Project/NopCommerce.java
@@ -1,8 +1,15 @@
 package NopCommerce_Project;
 
 import Utility.BaseDriver;
+import org.testng.annotations.Test;
 
 public class NopCommerce extends BaseDriver {
+
+    @Test
+    public void US_504_Oguzhan(){
+
+
+    }
 
 
 }
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -42,6 +42,7 @@ public class BaseDriver {
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
