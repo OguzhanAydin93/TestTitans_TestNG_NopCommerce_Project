4555586d65bf3fccf8b252d55f5557f506c150ad
nurii <ernuri518@gmail.com>
2024-03-31 00:01:14 +0300

    Eksikler tamamlandı.

--- src/NopCommerce_Project/NopCommerce.java
+++ src/NopCommerce_Project/NopCommerce.java
@@ -2,11 +2,47 @@ package NopCommerce_Project;
 
 import Utility.BaseDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+
+
 public class NopCommerce extends BaseDriver {
+
+    @Test
+    public void US_501_Nuri(){
+        BaseDriver.driver.navigate().to("https://demo.nopcommerce.com/");
+
+
+        Nuri_POM elements = new Nuri_POM();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.register));
+        elements.register.click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements.gender));
+        elements.gender.click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements.firstname));
+        elements.firstname.sendKeys("Teamtitans");
+        wait.until(ExpectedConditions.elementToBeClickable(elements.lastname));
+        elements.lastname.sendKeys("Techno123");
+        Select day = new Select(elements.day);
+        day.selectByIndex(5);
+        Select month = new Select(elements.month);
+        month.selectByValue("5");
+        Select year = new Select(elements.year);
+        year.selectByIndex(1);
+        wait.until(ExpectedConditions.elementToBeClickable(elements.email));
+        elements.email.sendKeys("techno+1@gmail.com");
+        wait.until(ExpectedConditions.elementToBeClickable(elements.companyname));
+        elements.companyname.sendKeys("TestTitans");
+        wait.until(ExpectedConditions.elementToBeClickable(elements.password));
+        elements.password.sendKeys("123456");
+        wait.until(ExpectedConditions.elementToBeClickable(elements.confirmpassword));
+        elements.confirmpassword.sendKeys("123456");
+        wait.until(ExpectedConditions.elementToBeClickable(elements.registerButton));
+        elements.registerButton.click();
+    }
 
 
 
