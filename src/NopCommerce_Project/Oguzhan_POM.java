7fd64d3d3f82ca404e0c56808aa8e5bf3e5ccff3
Degisiklikler1 <megaozi9517536@gmail.com>
2024-03-30 14:44:36 +0300

    login, email, password, loginButton locater'ları eklendi

--- src/NopCommerce_Project/Oguzhan_POM.java
+++ src/NopCommerce_Project/Oguzhan_POM.java
@@ -1,7 +1,22 @@
 package NopCommerce_Project;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class Oguzhan_POM {
+
+    @FindBy(linkText = "Log in")
+    public WebElement login;
+
+    @FindBy(css = "[id='Email']")
+    public WebElement email;
+
+    @FindBy(css = "[id='Password']")
+    public WebElement password;
+
+    @FindBy(css = "[class='button-1 login-button']")
+    public WebElement loginButton;
+
 
 }
