1dbc12f22bdd962fc624038fa8e4f748396cf89c
ZGK <zehragkrl@gmail.com>
2024-03-31 20:16:13 +0300

    elemenrler update edildi, login method oluşturuldu.

--- src/NopCommerce_Project/Zehra_POM.java
+++ src/NopCommerce_Project/Zehra_POM.java
@@ -4,6 +4,7 @@ import Utility.BaseDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Zehra_POM {
     public Zehra_POM() {
@@ -17,6 +18,19 @@ public class Zehra_POM {
     public WebElement password;
     @FindBy(css = "[class='button-1 login-button']")
     public WebElement submitLogin;
+
+    public void Login(){
+        BaseDriver baseDriver=new BaseDriver();
+
+       baseDriver.myClick(loginButton);
+       BaseDriver.wait.until(ExpectedConditions.urlContains("login"));
+       baseDriver.mySendKeys(email,"techno+1@gmail.com");
+       baseDriver.mySendKeys(password,"123456");
+       baseDriver.myClick(submitLogin);
+       BaseDriver.wait.until(ExpectedConditions.urlToBe("https://demo.nopcommerce.com/"));
+
+
+    }
 
 
 }
