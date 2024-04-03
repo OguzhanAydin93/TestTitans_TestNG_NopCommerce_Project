79ea0cd9de0616c958e073dc7b25e92dae84c1ad
MertOzmen <avmertozmen93@gmail.com>
2024-04-03 22:29:27 +0300

    Değişiklikler

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -29,84 +28,82 @@ public class BaseDriver {
 
     @BeforeClass
     @Parameters("browserTipi")
-    public void BaslangicIslemleri() {
-        String browserTipi {
-
-            switch (browserTipi.toLowerCase()) {
-                case "chrome":
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    driver = new FirefoxDriver();
-                    break;
-                case "edge":
-                    driver = new EdgeDriver();
-                    break;
-                default:
-                    driver = new ChromeDriver();
-                    break;
+    public void BaslangicIslemleri(String browserTipi) {
 
 
+        switch (browserTipi.toLowerCase()) {
+            case "chrome":
                 driver = new ChromeDriver();
-                driver.manage().window().maximize();
-                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-                wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-
-            }
-
-            public void myClick (WebElement element){
-                wait.until(ExpectedConditions.elementToBeClickable(element));
-                scrollToElement(element);
-                element.click();
-
-            }
-
-            public void mySendKeys (WebElement element, String text){
-                wait.until(ExpectedConditions.visibilityOf(element));
-                scrollToElement(element);
-                element.clear();
-                element.sendKeys(text);
-            }
-
-            public void scrollToElement (WebElement element){
-                JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
-                js.executeScript("arguments[0].scrollIntoView();", element);
-            }
-
-            public void myJSClick (WebElement element){
-                JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
-                js.executeScript("arguments[0].click();", element);
-            }
-
-            public void myAssert (WebElement element, String text){
-                wait.until(ExpectedConditions.visibilityOf(element));
-                scrollToElement(element);
-                Assert.assertTrue(element.getText().contains(text), "Bu Sayfada Değilsiniz");
-            }
-
-
-            @AfterClass
-            public void KapanisIslemleri () {
-                driver.quit();
-
-                logger.info("Driver kapatıldı");
-            }
-
-            @BeforeMethod
-            public void BeforeMetod () {
-                logger.info("Metod başladı");
-
-                logger.warn("WARN : Metod başladı, hata oluşmuş olsa idi");
-            }
-
-            @AfterMethod
-            public void AfterMetod (ITestResult sonuc){
-                logger.info(sonuc.getName() + " Metod bitti " + (sonuc.getStatus() == 1 ? "Passed" : "failed"));
-
-                logger.warn("WARN : Metod bitti, hata oluşmuş olsa idi");
-            }
-
-
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+                break;
         }
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+
+    }
+
+    public void myClick(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        scrollToElement(element);
+        element.click();
+
+    }
+
+    public void mySendKeys(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void myJSClick(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public void myAssert(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        scrollToElement(element);
+        Assert.assertTrue(element.getText().contains(text), "Bu Sayfada Değilsiniz");
+    }
+
+
+    @AfterClass
+    public void KapanisIslemleri() {
+        driver.quit();
+
+        logger.info("Driver kapatıldı");
+    }
+
+    @BeforeMethod
+    public void BeforeMetod() {
+        logger.info("Metod başladı");
+
+        logger.warn("WARN : Metod başladı, hata oluşmuş olsa idi");
+    }
+
+    @AfterMethod
+    public void AfterMetod(ITestResult sonuc) {
+        logger.info(sonuc.getName() + " Metod bitti " + (sonuc.getStatus() == 1 ? "Passed" : "failed"));
+
+        logger.warn("WARN : Metod bitti, hata oluşmuş olsa idi");
+    }
+}
